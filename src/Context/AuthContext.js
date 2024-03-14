b9591ff016571e6b987cdd9c8068f862186ebb0e
Vikram9392 <148332548+Vikram9392@users.noreply.github.com>
2024-03-14 14:03:56 +0530

    Update AuthContext.js

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -1,1 +1,15 @@
+import { createContext, useState } from "react";
 
+const AuthContext = createContext(null);
+
+const AuthProvider = ({children})=>{
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [userDetails, setUserDetails] = useState(null);
+    return (
+        <AuthContext.Provider value = {{isLoggedIn, setIsLoggedIn, userDetails, setUserDetails}}>
+            {children}
+        </AuthContext.Provider>
+    )
+};
+
+export {AuthContext, AuthProvider};

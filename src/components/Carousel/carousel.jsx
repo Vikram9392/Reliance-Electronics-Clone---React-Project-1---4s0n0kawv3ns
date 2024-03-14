f75823c4f8c093db421461b7a7016453a9eb7ba0
Vikram9392 <148332548+Vikram9392@users.noreply.github.com>
2024-03-14 13:34:06 +0530

    Update carousel.jsx

--- src/components/Carousel/carousel.jsx
+++ src/components/Carousel/carousel.jsx
@@ -1,1 +1,37 @@
+import React from "react";
+import './carousel.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Link } from "react-router-dom";
+import Slider from "react-slick";
+import SliderCard from "../SliderCard/SliderCard";
 
+
+const Carousel = (props) => {
+  const {config, sliderList, classes} = props;
+  // console.log(sliderList);
+  return (
+    <>
+      <Slider className={classes ? classes : null} {...config}>
+        {
+          sliderList && 
+          Array.isArray(sliderList) && 
+          sliderList.map((listItem, index)=>{
+            if(listItem.imageUrl){
+              return (
+                <Link to={listItem.path} key={listItem.id}><div className="carousel-img-container"><img src={listItem.imageUrl} alt={listItem.displayName} /></div></Link>
+              )
+            }
+            else {
+              return (
+                <SliderCard product={listItem} key={listItem._id}/>
+              )
+            }
+          }) 
+        }
+      </Slider>
+    </>
+  );
+};
+
+export default Carousel;

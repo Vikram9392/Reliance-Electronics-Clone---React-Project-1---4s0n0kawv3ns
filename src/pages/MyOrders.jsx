8bd2caf61845a6ad005ddcf07c6f1f2a3e105467
Vikram9392 <148332548+Vikram9392@users.noreply.github.com>
2024-03-14 14:12:51 +0530

    Update MyOrders.jsx

--- src/pages/MyOrders.jsx
+++ src/pages/MyOrders.jsx
@@ -1,1 +1,27 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
+function MyOrders() {
+  const navigate = useNavigate();
+  const [purchasedProducts, setPurchasedProducts] = useState([]);
+  function handleEmptyOrderPageBtnClick() {
+    navigate("/");
+  }
+  return (
+    <div className='order-history-page'>
+        {
+          purchasedProducts && purchasedProducts.length == 0 ? 
+          (<div className='empty-order-page flex'>
+            <div className='empty-order-img-text-container flex'>
+              <img src="/no-item-found.png" alt="no orders found" />
+              <p>No Orders Found</p>
+            </div>
+            <button className='empty-order-page-btn' onClick={handleEmptyOrderPageBtnClick}>continue shopping</button>
+          </div>) :
+          (<div>some products</div>)
+        }
+    </div>
+  )
+}
+
+export default MyOrders;

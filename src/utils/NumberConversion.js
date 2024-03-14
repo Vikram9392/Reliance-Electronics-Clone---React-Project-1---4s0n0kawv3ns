6b648e495fbad525a7792f406c334079abbfa834
Vikram9392 <148332548+Vikram9392@users.noreply.github.com>
2024-03-14 14:20:03 +0530

    Update NumberConversion.js

--- src/utils/NumberConversion.js
+++ src/utils/NumberConversion.js
@@ -1,1 +1,11 @@
+export const INRConversion = (price)=>{
+    let rupee = new Intl.NumberFormat("en-IN", {
+        style: 'currency',
+        currency: 'INR',        
+    })
+    let formattedResult = rupee.format(price);
+    return formattedResult;
+};
+
+
 

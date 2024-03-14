c8c571f3616c755ae9d0879685a4787418f4a2e4
Vikram9392 <148332548+Vikram9392@users.noreply.github.com>
2024-03-14 14:04:10 +0530

    Update CartContext.js

--- src/Context/CartContext.js
+++ src/Context/CartContext.js
@@ -1,1 +1,12 @@
+import { createContext, useState } from "react";
 
+export const CartContext = createContext(null);
+
+export const CartProvider = ({children})=>{
+    const [cartCount, setCartCount] = useState("");
+    return (
+        <CartContext.Provider value={{cartCount, setCartCount}}>
+            {children}
+        </CartContext.Provider>
+    )
+};

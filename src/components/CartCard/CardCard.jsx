7d0d560f9182faf5346be7a3a2188f8574841980
Vikram9392 <148332548+Vikram9392@users.noreply.github.com>
2024-03-14 13:36:22 +0530

    Update CardCard.jsx

--- src/components/CartCard/CardCard.jsx
+++ src/components/CartCard/CardCard.jsx
@@ -1,1 +1,38 @@
+import React from 'react'
+import './CartCard.css'
+import { Link } from 'react-router-dom';
 
+function CartCard(props) {
+    const {item, removeProduct, addProductToWishlist} = props;
+    
+  return (
+    <div className='cart-card'>
+        <div className='cart-product-details'>    
+            <div className='cart-img-container'>
+                <Link to={`/product/${item.product._id}`}><img 
+                    className='cart-image'
+                    src={item.product.displayImage} 
+                    alt={item.product.name} 
+                    onError={(e)=>{
+                        e.currentTarget.src = "/placeholder.png"
+                    }} 
+                /></Link>
+            </div>
+            <div className='cart-product-title-review'>
+                <Link to={`/product/${item.product._id}`}><div className='cart-product-title'>{item.product.name}</div></Link>
+            </div>
+            <div className='cart-product-price'>₹{(item.product.price).toLocaleString("en-IN")}</div>
+        </div>
+        <div className='cart-item-qty-container'>
+            <div>Qty: </div>
+            <div>{item.quantity}</div>
+        </div>
+        <div className='cart-item-control'>
+            <button className='cart-item-remove-btn' onClick={removeProduct}>remove</button>
+            <button className='cart-item-wishlist-btn' onClick={addProductToWishlist}>move to wishlist</button>
+        </div>
+    </div>
+  )
+}
+
+export default CartCard;
